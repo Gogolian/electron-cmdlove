05f4d0b7d8ca1e1a696bac976c3e3f949cc1515d
Kamil Wężyk <kamilw@pro51.pl>
2019-06-27 09:50:21 +0200

    Modularity v1 - code refactor

--- main.js
+++ main.js
@@ -1,53 +1,88 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, Tray, Menu } = require('electron')
+
+const { spawn, exec } = require('child_process');
+
+
 const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const iconpath = path.join(__dirname, 'user.ico')
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+let tray = null
+
+app.on('ready', () =>{
+
+  tray = new Tray(iconpath)
+
+  const conemu = "C:\\Program Files\\ConEmu\\ConEmu64.exe"
+
+  const runcmd = ['-run', 'cmd', '/k', '-cur_console:',]
+
+  const ops = {
+    detached: true,
+    stdio: 'ignore'
+  }
+
+  let separator = true, quit = true;
+
+  const menu = [
+    {
+      separator
+    },
+    {
+      task: '{sam-api}'
+    },
+    {
+      task: '{sam-docker-compose-up}'
+    },
+    {
+      task: '{sam-docker-compose-down}'
+    },
+    {
+      cmd: 'docker ps -a'
+    },
+    {
+      separator
+    },
+    {
+      text: 'Show ConEmu',
+      cmd: '-cur_console:n exit'
+    },
+    {
+      text: 'New Console',
+      cmd: '-new_console:echo "Welcome!"'
+    },
+    {
+      separator
+    },
+    {
+      quit
     }
+  ]
+
+
+  const contextMenu = Menu.buildFromTemplate(menu.map((mi)=>{
+
+    if(mi.quit) return { role: 'quit' }
+    if(mi.separator) return { type: 'separator' }
+
+    let label = mi.text || mi.cmd || mi.task || 'No Name';
+
+    let command = mi.cmd ? [...runcmd, mi.cmd] :  mi.task ? ['-run', mi.task] : [] 
+
+    return {
+        label,
+        click: function () {
+          spawn(conemu, command, ops)
+        }
+    }
+
+  }));
+
+  tray.setContextMenu(contextMenu)
+
+  tray.on('click', () => {
+    spawn(conemu, [...runcmd, '-cur_console:n exit'], ops)
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
